aa911c4db7acded8d6c1923153609dbdd3a8ae09
James Englander <jenglander25@gmail.com>
2021-05-19 15:19:10 -0400

    Made bot file that boops people

---
+++ discordBot.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from discord.ext import commands
+
+
+
+# get the environmental variable for the discord user
+load_dotenv()  # load from the .en folder all thje environmental variables
+BOTTOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+# create connection object
+bot = commands.Bot(command_prefix='!')
+
+# bot command to boop users
+@bot.command(name='boop')
+async def boop(ctx):
+    await ctx.send('boop')
