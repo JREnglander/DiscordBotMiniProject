3ee6cc77d4d7fa75c88a7bbd399ab629c7ac8b3b
James Englander <jenglander25@gmail.com>
2021-05-19 22:06:30 -0400

    Modified discord tutorial bot and created main file for weatherbot

---
+++ weatherBot.py
@@ -0,0 +1,24 @@
+import os
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands, tasks
+import random
+
+
+# get the environmental variable for the discord user
+load_dotenv()  # load from the .en folder all thje environmental variables
+BOTTOKEN = os.getenv('DISCORD_TOKEN')
+
+# create connection object
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} is connected to Discord')
+
+# TODO: Figure out how to access and make HTTP requests to Weather.com API (might be separate class)
+# TODO: Figure out how to use tasks to make timely alerts
+# TODO: Make commands to get weather for certain place
+
+# run the bot
+bot.run(BOTTOKEN)
