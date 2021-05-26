7eac04772d88ab2e6b82b1efdd8b80df6bbf2186
JREnglander <56492290+JREnglander@users.noreply.github.com>
2021-05-26 11:47:01 -0400

    Added params to requestEdamam and made custom error for base Requester class

--- Requester.py
+++ Requester.py
@@ -1,4 +1,5 @@
 # base request class to use to simplify code
+from typing_extensions import ParamSpec
 import requests
 import json
 
@@ -6,9 +7,22 @@ class Requester:
     def __init__(self) -> None:
         pass
     
-    def makeGETRequest(fullURL, codeDictKey):
+    def makeGETRequest(apiBaseURL, headers=None, params=None, codeDictKey=0):
+        """
+        apiBaseURL: base url to make get requests
+        headers: values to add to header of request
+        params: values to add to body of request
+        """
         # get the response as a python dictionary
-        response = json.loads(requests.get(fullURL))
+        response = None
+        if headers == None or params == None:
+            if headers == None:
+                response = json.loads(requests.get(apiBaseURL,params=params))
+            elif params == None:
+                # raise error since request cannot have no params
+                raise noParamsRequestError
+        else:
+            response = json.loads(requests.get(apiBaseURL, headers, params))
         # handle the reaponse code here ideally
         # the codeDictkey is the key in the response dictionary
         # since various APIs might call it different things, 
@@ -18,10 +32,19 @@ class Requester:
             return response
         elif responseCode == 404:
             return 404
-        else:
+        elif responseCode >= 500:
             return 500
+        else: 
+            # could not find the response code
+            return 0
         
     
-    ## might not need further request types in this stage (pre raspberry pi recipe server)
+    ## might not need further request types in this stage (post raspberry pi recipe server)
     def makePOSTRequest():
         pass
+
+class noParamsRequestError(Exception):
+    """
+    Raised when no params are given for a request
+    """
+    pass
--- foodRequester.py
+++ foodRequester.py
@@ -15,7 +15,34 @@ class foodRequester(Requester):
     # TODO: understand search query parameters to make a get request
     # there are alot
     # issue #7 Github    
-    def requestEdamam():
+    def requestEdamam(queryOrId, startIndex, endIndex, 
+                      numIngreds, dietLabel, healthLabel, 
+                      cuisineType, mealType, dishType, 
+                      calories, time, excluded):
+        """
+        queryOrId: the search term to define the request (required)
+        all of other parameters are optional:
+        startIndex and endIndex: parameters to define where to pull recipes (prevents pulling entire database each time)
+        numIngreds: number of ingredients to make recipe
+        the following params are enums with string labels defined on edamam docs
+        dietLabel, healthLabel, cuisineType, mealType, dishType
+        The following params have a range type which comes in three forms:
+        MIN+, MIN-MAX, or MAX  
+        calories
+        time
+        excluded: param that takes strings of ingredients to exclude from search
+        """
+
+        # TODO: handle each parameter 
+        # TODO: Required parameters: queryOdrId, startIndex, endIndex
+        # TODO: Handle list/enum Params: label, type params
+        # TODO: Handle Range Params
+        # TODO: excluded should be a list and you generate a string with the list 
+        # TODO: generate final URL 
+        # TODO: make request
+
+        # TODO: create config file for params to keep constant in requests
+
         pass
     
     
